4094e4e0a5d39c8dc87f31370ad5a4e6913cb4db
Gleb <git@gleb.nl>
2016-11-01 22:47:40 +0100

    Enable transparency on the renderer, set clear color to default.

--- src/app.js
+++ src/app.js
@@ -12,10 +12,10 @@ render();
 
 function init () {
 
-  renderer = new THREE.WebGLRenderer( { antialias: true } );
+  renderer = new THREE.WebGLRenderer( { antialias: true, alpha: true } );
   renderer.setPixelRatio( window.devicePixelRatio );
   renderer.setSize( window.innerWidth, window.innerHeight );
-  renderer.setClearColor( 0x000000, 1 );
+  renderer.setClearColor( 0x000000, 0 );
   document.body.appendChild( renderer.domElement );
 
   scene = new THREE.Scene();
