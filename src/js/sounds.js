01528441cecda58885edb0fc5eefe368d96f95eb
Gleb <git@gleb.nl>
2016-11-12 11:22:24 +0100

    Prefetch all sounds on init and assign them to the buffers array - ready for playback.

--- src/js/sounds.js
+++ src/js/sounds.js
@@ -2,53 +2,48 @@
 
 var sounds = {
 
-  source: undefined,
+  buffers: [
+    'src/sounds/0_gun.mp3',
+    'src/sounds/1_khaled.mp3',
+    'src/sounds/2_chicken.mp3',
+    'src/sounds/3_cops.mp3',
+    'src/sounds/4_airhorn.mp3',
+    'src/sounds/5_laser.mp3',
+    'src/sounds/6_snare.mp3',
+    'src/sounds/7_snap.mp3',
+    'src/sounds/8_clap.mp3',
+    'src/sounds/9_kick.mp3',
+  ],
+
   context: undefined,
 
   init: function () {
     sounds.context = new (window.AudioContext || window.webkitAudioContext)();
+    sounds.fetchSoundBuffers();
   },
 
   play: function (id) {
-    sounds.source = sounds.context.createBufferSource();
-    sounds.source.connect(sounds.context.destination);
-    sounds.getSoundBuffer(sounds.getSoundUrl(id));
+    var source = sounds.context.createBufferSource();
+    source.connect(sounds.context.destination);
+    source.buffer = sounds.buffers[id-48];
+    source.start(0);
   },
 
-  getSoundBuffer: function (url) {
-    var request = new XMLHttpRequest();
-    request.open('GET', url, true);
-    request.responseType = 'arraybuffer';
-    request.onload = function() {
-      sounds.context.decodeAudioData(request.response, function(buffer) {
-        sounds.source.buffer = buffer;
-        sounds.source.start(0);
-      }, function () {});
-    }
-    request.send();
-  },
-
-  getSoundUrl: function (id) {
-    if ( id === 48 ) {
-      return 'src/sounds/0_gun.mp3';
-    } else if ( id === 49 ) {
-      return 'src/sounds/1_khaled.mp3';
-    } else if ( id === 50 ) {
-      return 'src/sounds/2_chicken.mp3';
-    } else if ( id === 51 ) {
-      return 'src/sounds/3_cops.mp3';
-    } else if ( id === 52 ) {
-      return 'src/sounds/4_airhorn.mp3';
-    } else if ( id === 53 ) {
-      return 'src/sounds/5_laser.mp3';
-    } else if ( id === 54 ) {
-      return 'src/sounds/6_snare.mp3';
-    } else if ( id === 55 ) {
-      return 'src/sounds/7_snap.mp3';
-    } else if ( id === 56 ) {
-      return 'src/sounds/8_clap.mp3';
-    } else if ( id === 57 ) {
-      return 'src/sounds/9_kick.mp3';
+  fetchSoundBuffers: function () {
+    for ( var i = 0; i < 10; i++ ) {
+      (function(i) {
+        var request = new XMLHttpRequest();
+        request.open('GET', sounds.buffers[i], true);
+        request.responseType = 'arraybuffer';
+        request.onload = function (data) {
+          sounds.context.decodeAudioData(request.response, function(buffer) {
+            sounds.buffers[i] = buffer;
+          }, function (err) {
+            console.log('sounds err: ', err);
+          });
+        }
+        request.send();
+      })(i);
     }
   },
 
